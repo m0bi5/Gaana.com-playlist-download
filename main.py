0dc113b749f0013d195c27e2317be0eb315e5b37
Mohit Bhasi <mohit.bhasi@yahoo.com>
2016-04-10 00:44:30 +0300

    final commit

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ import time
 ########################################################################
 chromeOptions = webdriver.ChromeOptions()
 pref = {"download.default_directory" : "C:\\CarMusic"}                                                                            #Change default download folder
-
 chromeOptions.add_experimental_option("prefs",pref)
 browser=webdriver.Chrome(executable_path="C:\\chromedriver.exe",chrome_options=chromeOptions)
 browser.get("http://gaana.com/playlist/neemabhasi-bxxha-carmusic")                                                                #Link to playlist
@@ -55,7 +54,7 @@ browser.get("http://gaana.com/playlist/neemabhasi-bxxha-carmusic")              
 def get():
     song_elem=browser.find_elements_by_class_name("playlisttrack")
     song_list=[]
-    for x in range(0,len(songs)):
+    for x in range(0,len(song_elem)):
         song_list.append(song_elem[x].text.split("\n")[0])
     song_list.pop(0)
     return song_list
@@ -66,8 +65,8 @@ def get():
 def download(s):
     link="https://www.youtube.com/results?search_query="+s
     browser.get(link)
-    browser.execute("document.cookie=\"VISITOR_INFO1_LIVE=oKckVSqvaGw; path=/; domain=.youtube.com\";window.location.reload();")  #Disable ads
-    browser.find_elements_by_class_name("yt-ui-ellipsis")[0].click()  
+    browser.execute_script("document.cookie=\"VISITOR_INFO1_LIVE=oKckVSqvaGw; path=/; domain=.youtube.com\";window.location.reload();")  #Disable ads
+    browser.find_elements_by_class_name("yt-ui-ellipsis")[0].click()
     be=browser.current_url
     browser.get("https://www.youtube2mp3.cc/")
     t=browser.find_element_by_id("input")
